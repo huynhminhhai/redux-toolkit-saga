dde5a97230f9c33f463b11dfbaa938ed9840d62f
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-18 17:26:13 +0700

    [UPDATE] incrementSync with saga

--- src/features/counter/CounterSaga.ts
+++ src/features/counter/CounterSaga.ts
@@ -1,12 +1,21 @@
 import { PayloadAction } from '@reduxjs/toolkit'
-import { increment } from 'features/counter/counterSlice'
-import { takeEvery } from 'redux-saga/effects'
+import { incrementByAmountSaga, incrementByAmountSagaSuccess } from 'features/counter/counterSlice'
+import { delay, put, takeEvery } from 'redux-saga/effects'
 
-export function* log(action: PayloadAction) {
-  console.log('Log: ', action)
+// export function* log(action: PayloadAction) {
+//   console.log('Log: ', action)
+// }
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log('waiting 1s')
+  // wait 1s
+  yield delay(1000)
+
+  console.log('wating done, dispatch action')
+  // Dispatch action success
+  yield put(incrementByAmountSagaSuccess(action.payload))
 }
 
 export default function* counterSaga() {
   console.log('Counter Saga')
-  yield takeEvery(increment().type, log)
+  yield takeEvery(incrementByAmountSaga.toString(), handleIncrementSaga)
 }
