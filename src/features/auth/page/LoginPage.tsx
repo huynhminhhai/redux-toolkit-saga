f615521ca9b422baff980d30252ff4f748ef2284
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-19 11:26:57 +0700

    [UPDATE] setup mui and build ui login page

--- src/features/auth/page/LoginPage.tsx
+++ src/features/auth/page/LoginPage.tsx
@@ -1,5 +1,33 @@
+import { Box, Button, Container, Theme, Typography } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+
+const useStyles: any = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh'
+  }
+}))
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth='xl'>
+        <Typography variant='h5' component='h1' color='primary' textAlign='center'>
+          Student Management
+        </Typography>
+        <Box mt={4} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Button variant='contained' color='primary'>
+            Fake Login
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  )
 }
 
 export default LoginPage
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import { store } from './app/store'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
+const theme = createTheme()
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
