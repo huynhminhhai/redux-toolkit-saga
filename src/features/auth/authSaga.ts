f6dd33c01f38e7bac5729a48d59f1a8e1c7773dd
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-19 12:23:31 +0700

    [UPDATE] create task login and task logout

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,22 +1,41 @@
 import { PayloadAction } from '@reduxjs/toolkit'
 import { LoginPayload, authActions } from 'features/auth/authSlice'
-import { fork, take } from 'redux-saga/effects'
+import { call, delay, fork, put, take } from 'redux-saga/effects'
 
 function* handleLogin(payload: LoginPayload) {
-  console.log('handleLogin', payload)
+  try {
+    // call api login
+    yield delay(1000)
+    localStorage.setItem('access_token', '123')
+    // redirect to admin page
+    yield put(
+      authActions.loginSuccess({
+        id: 1,
+        name: 'huynhminhhai'
+      })
+    )
+  } catch (error: any) {
+    yield put(authActions.loginFailed(error.message))
+  }
 }
 
 function* handleLogout() {
-  console.log('handleLogout')
+  // call api logout
+  yield delay(500)
+  localStorage.removeItem('access_token')
+  // redirect to login page
 }
 
 function* watchLoginFlow() {
   while (true) {
-    const action: PayloadAction<LoginPayload> = yield take(authActions.login.type)
-    yield fork(handleLogin, action.payload)
-
-    yield take(authActions.logout.type)
-    yield fork(handleLogout)
+    const isLoggedIn = Boolean(localStorage.getItem('access_token'))
+    if (!isLoggedIn) {
+      const action: PayloadAction<LoginPayload> = yield take(authActions.login.type)
+      yield call(handleLogin, action.payload)
+    } else {
+      yield take(authActions.logout.type)
+      yield call(handleLogout)
+    }
   }
 }
 
