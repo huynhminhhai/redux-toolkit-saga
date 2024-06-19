214e2b290671769d0ace1e07968a5262fe1a3ffa
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-06-19 08:43:59 +0700

    [UPDATE] setup APIs for students

--- src/api/studentsApi.ts
+++ src/api/studentsApi.ts
@@ -1,4 +1,33 @@
-class StudentApi {}
+import axiosClient from 'api/axiosClient'
+import { ListParams, ListRespone } from 'models'
+import { Student } from 'models/student'
+
+class StudentApi {
+  getAll(params: ListParams): Promise<ListRespone<Student>> {
+    const url = '/students'
+    return axiosClient.get(url, { params })
+  }
+
+  getById(id: string): Promise<Student> {
+    const url = `/students/${id}`
+    return axiosClient.get(url)
+  }
+
+  add(data: Student): Promise<Student> {
+    const url = '/students'
+    return axiosClient.post(url, data)
+  }
+
+  update(data: Student): Promise<Student> {
+    const url = '/students'
+    return axiosClient.patch(url, data)
+  }
+
+  remove(id: string): Promise<ListRespone<Student>> {
+    const url = `/students/${id}`
+    return axiosClient.delete(url)
+  }
+}
 
 const studentApi = new StudentApi()
 export default studentApi
